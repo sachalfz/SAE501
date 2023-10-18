8a15762468adf007dd8a4c42ff5a3ec13533e06a
Sacha LEFETZ <sacha.lefetz@gmail.com>
2023-10-18 15:29:24 +0200

    suite jeu solo

--- sae501/src/components/SoloGuesses.vue
+++ sae501/src/components/SoloGuesses.vue
@@ -1,11 +1,26 @@
 <template>
     <div class="sologuesses">
-        <!-- <p class="sologuesses--txt">Results will be shown here</p> -->
-        <div v-for="(album, index) in selectedAlbums" :key="index" class="sologuesses--album">
-            <div class="sologuesses--self"><p class="sologuesses--self--txt">Nom de l'album : {{ album.nom }}</p></div>
-            <div class="sologuesses--self"><p class="sologuesses--self--txt">Année : {{ album.date }}</p></div>
-            <div class="sologuesses--self"><p class="sologuesses--self--txt">Genre : {{ album.genre }}</p></div>
-            <div class="sologuesses--self"><p class="sologuesses--self--txt">Label : {{ album.label }}</p></div>
+        <div class="sologuesses--categories">
+            <div class="sologuesses--categories--self"><p class="sologuesses--self--txt">Nom de l'album</p></div>
+            <div class="sologuesses--categories--self"><p class="sologuesses--categories--self--txt">Artiste</p> </div>
+            <div class="sologuesses--categories--self"><p class="sologuesses--categories--self--txt">Année</p></div>
+            <div class="sologuesses--categories--self"><p class="sologuesses--categories--self--txt">Genre</p></div>
+            <div class="sologuesses--categories--self"><p class="sologuesses--categories--self--txt">Label</p></div>
+            <div class="sologuesses--categories--self"><p class="sologuesses--categories--self--txt">Certification</p></div>
+            <div class="sologuesses--categories--self"><p class="sologuesses--categories--self--txt">Beatmakers</p></div>
+        </div>
+        <div class="sologuesses--results">
+            <div v-for="(album, index) in selectedAlbums" :key="index" class="sologuesses--album">
+                <div class="sologuesses--self"><p class="sologuesses--self--txt">{{ album.nom }}</p></div>
+                <div class="sologuesses--self"><p class="sologuesses--self--txt">{{ album.artiste }}</p></div>
+                <div class="sologuesses--self"><p class="sologuesses--self--txt">{{ album.date }}</p></div>
+                <div class="sologuesses--self"><p class="sologuesses--self--txt">{{ album.genre }}</p></div>
+                <div class="sologuesses--self"><p class="sologuesses--self--txt">{{ album.label }}</p></div>
+                <div class="sologuesses--self"><p class="sologuesses--self--txt">{{ album.certification }}</p></div>
+                <div class="sologuesses--self" v-for="(beatmakers, index) in album.beatmakers" :key="index">
+                    <p class="sologuesses--self--txt">{{ beatmakers }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
