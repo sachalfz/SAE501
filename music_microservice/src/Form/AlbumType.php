27fef0991b0c352ab10991e862e926f159822b1b
Sacha LEFETZ <sacha.lefetz@gmail.com>
2023-11-09 19:04:51 +0100

    eee

--- music_microservice/src/Form/AlbumType.php
+++ music_microservice/src/Form/AlbumType.php
@@ -19,20 +19,19 @@ class AlbumType extends AbstractType
             ->add('date')
             ->add('label')
             ->add('beatmakers', CollectionType::class, [
-                'entry_type' => TextType::class, // Assuming beatmakers are represented as strings
                 'allow_add' => true,
                 'allow_delete' => true,
-                'by_reference' => false, // This is important to properly handle the collection
+                'by_reference' => false,
             ])
             ->add('cover')
-            ->add('certification')
-        ;
+            ->add('certification');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Album::class,
+            'csrf_protection' => false,
         ]);
     }
 }
