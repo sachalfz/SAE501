28645cf648953ba6925196f329edc53ad6e6f2c3
Louis Maury <118908672+LouisHM@users.noreply.github.com>
2023-12-12 14:43:40 +0100

    Update CanvaThreeD.vue

--- sae501/src/components/CanvaThreeD.vue
+++ sae501/src/components/CanvaThreeD.vue
@@ -7,28 +7,12 @@ import { createScene } from '../script/script.js';
 
 export default {
   mounted() {
-    createScene();
-
-    const gsapScript = document.createElement('script');
-    gsapScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js';
-    gsapScript.async = true;
-
-    const esModuleShimsScript = document.createElement('script');
-    esModuleShimsScript.src = 'https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js';
-    esModuleShimsScript.async = true;
-
-    // Chargement des scripts
-    document.head.appendChild(gsapScript);
-    document.head.appendChild(esModuleShimsScript);
-
-    // Attendez que les scripts soient chargés avant d'effectuer des opérations liées à GSAP ou es-module-shims.
-    gsapScript.onload = () => {
-      // Votre code GSAP ici
-    };
-
-    esModuleShimsScript.onload = () => {
-      // Votre code lié à es-module-shims ici
-    };
+    this.createThree();
+  },
+  methods: {
+    async createThree() {
+      await createScene();
+    }
   }
 };
 </script>
