6eae1e175f6bcc4a3d4bb9699bdab8c97fa354b7
Sacha LEFETZ <sacha.lefetz@gmail.com>
2024-01-12 09:41:45 +0100

    helper avec pop up pour les règles du jeu solo

--- sae501/src/components/SoloRules.vue
+++ sae501/src/components/SoloRules.vue
@@ -1,4 +1,8 @@
 <template>
+    <div class="rules" @click="openPopupRules">
+        <p class="rules-txt" style="cursor: pointer;">Guess an album by its criteria !</p>
+        <font-awesome-icon icon="question-circle" class="rules-svg"> </font-awesome-icon>
+    </div>
     <div class="solo-rules">
         <div class="rules-bloc true">
             <p class="bloc-txt">True</p>
@@ -11,5 +15,49 @@
         </div>
     </div>
 
-    <p class="rules-txt">Guess an album by its criteria !</p>
+    <div v-if="popupRulesVisible" class="popuprules">
+        <div class="popuprules--header">
+            <p class="popuprules--header--title">Guess a Rap Album</p>
+            <font-awesome-icon icon="times" class="close--icon" @click="closePopupRules"/>
+        </div>
+        <p class="popuprules--description">Enter a random album to begin. Next, with the clues you got by choosing one, try to find the right album and win the game !</p>
+        <div class="popuprules--lifes">
+            <div class="popuprules--lifes--group">
+                <img src="../assets/icons/disk_full.svg" alt="disk full" class="popuprules--lifes--icon">
+                <img src="../assets/icons/disk_broken.svg" alt="disk full" class="popuprules--lifes--icon">
+            </div>
+            <p class="popuprules--lifes--txt">You have 10 lives per game, so choose well !</p>
+        </div>
+    </div>
+
 </template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+// Dans votre fichier script
+library.add(faQuestionCircle, faTimes);
+
+export default {
+    name: 'SoloRules',
+    data() {
+        return {
+            popupRulesVisible: false,
+        }
+    },
+    methods: {
+        openPopupRules() {
+            this.popupRulesVisible = true;
+        },
+        closePopupRules() {
+            this.popupRulesVisible = false;
+        }
+    },
+    components: {
+        FontAwesomeIcon
+    }
+}
+</script>
