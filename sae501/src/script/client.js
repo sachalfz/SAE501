c9d467b6a5719979d26463f6fa15d091d21e5769
Sacha LEFETZ <sacha.lefetz@gmail.com>
2024-01-16 12:20:05 +0100

    socket 3000

--- sae501/src/script/client.js
+++ sae501/src/script/client.js
@@ -59,7 +59,7 @@ export async function createScene() {
   const GRAVITY = 30;
   const STEPS_PER_FRAME = 5;
 
-  const socket = io("https://rapguess-server.mmi-limoges.fr:3000");
+  const socket = io("https://rapguess-server.mmi-limoges.fr");
 
   // Paramètres de camera
   const viewpointCamera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
